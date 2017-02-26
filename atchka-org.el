3272113cb479fedc8a4f19e44b6716893e743c28
dwcoates <dodge.w.coates@gmail.com>
2017-02-26 01:29:05 -0500

    Fix face protection

--- atchka-org.el
+++ atchka-org.el
@@ -164,9 +164,8 @@ Please `previous-line' past org-block headers'"
     (let ((face (get-text-property (point) 'face))
           (next (next-single-property-change (point) 'face nil end)))
       (cond
-       ((memqr face '(org-block-begin-line
-                      org-block-end-line
-                      atchka-org-source-block-face))
+       ((memq face '(org-block-begin-line
+                     org-block-end-line))
         (let ((ov (make-overlay (point) next)))
           (overlay-put ov 'priority protect-faces-priority)
           (overlay-put ov 'protect-faces t)
